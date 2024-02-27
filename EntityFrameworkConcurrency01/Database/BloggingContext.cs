db12cda86547bff4c930db7d304ca92dd681aaca
KBR <kbr@ucl.dk>
2024-02-27 17:26:50 +0100

    EntityFrameworkConcurrency01 - initial commit

---
+++ EntityFrameworkConcurrency01/Database/BloggingContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkConcurrency01.Database;
+
+public class BloggingContext : DbContext
+{
+    public BloggingContext(DbContextOptions<BloggingContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+}
---
+++ EntityFrameworkConcurrency01/Database/Model.cs
@@ -0,0 +1,16 @@
+﻿namespace EntityFrameworkConcurrency01.Database;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Url { get; set; }
+    public List<Post> Posts { get; } = [];
+}
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public Blog Blog { get; set; }
+}
